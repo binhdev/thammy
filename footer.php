21f30cfd62338f60b1e6dadc82a7a3604cd59a01
binh <ttbinh@hueic.edu.vn>
2019-04-03 20:54:13 +0700

    Finish convert from html to wordpress

--- footer.php
+++ footer.php
@@ -10,27 +10,101 @@
  */
 
 ?>
+</div>
+    <footer id="footer">
+      <div class="footer-logo">
+        <img class="normal_logo" src="<?php echo get_template_directory_uri() .'/media/images/footer-logo.png' ?>" title="Footer Logo">
+      </div>
+      <div class="footer-widgets-wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-md-4 col-sm-12 col-12 dt-sc-one-fourth first">
+                <aside id="nav_menu-2" class="widget widget_nav_menu">
+                  <h3 class="widgettitle">Links</h3>
+                  <div class="menu-footer-widget-menu-container">
+                    <ul id="menu-footer-widget-menu" class="menu">
+                      <li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-105 current_page_item menu-item-397">
+                        <a href="https://dreamspa.dttheme.com/">Home</a>
+                      </li>
 
-	</div><!-- #content -->
+                      <li id="menu-item-398" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-398"><a href="https://dreamspa.dttheme.com/about-us/">About us</a>
+                      </li>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'thammy' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'thammy' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'thammy' ), 'thammy', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+                      <li id="menu-item-399" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399">
+                        <a href="https://dreamspa.dttheme.com/blog/">Blog</a>
+                      </li>
 
-<?php wp_footer(); ?>
+                      <li id="menu-item-400" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-400"><a href="https://dreamspa.dttheme.com/gallery/">Gallery</a>
+                      </li>
 
+                      <li id="menu-item-401" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-401">
+                        <a href="https://dreamspa.dttheme.com/location/">Location</a>
+                      </li>
+                    </ul>
+                  </div>
+                </aside>
+              </div>
+
+              <div class="col-lg-3 col-md-4  col-sm-12 col-12 dt-sc-one-fourth ">
+                <aside id="text-4" class="widget widget_text">
+                  <h3 class="widgettitle">Services</h3>
+                  <div class="textwidget">
+                    <ul>
+                      <li> <a title="" href=""> Aromatheraphy </a> </li>
+                      <li> <a title="" href=""> Skin Care </a> </li>
+                      <li> <a title="" href=""> Herbal Spa </a> </li>
+                      <li> <a title="" href=""> Hydrotherapy </a> </li>
+                      <li> <a title="" href=""> Electropathy </a> </li>
+                    </ul>
+                  </div>
+                </aside>
+              </div>
+
+              <div class="col-lg-3 col-md-4  col-sm-12 col-12 dt-sc-one-fourth ">
+                <aside id="text-3" class="widget widget_text">
+                  <h3 class="widgettitle">Social</h3>
+                  <div class="textwidget">
+                    <ul class="dt-sc-social-icons">
+                      <li> <a title="" href=""> <i class="fa fa-facebook"> </i> Facebook </a> </li>
+                      <li> <a title="" href=""> <i class="fa fa-twitter"> </i> Twitter </a> </li>
+                      <li> <a title="" href=""> <i class="fa fa-google-plus"> </i> Google Plus </a> </li>
+                      <li> <a title="" href=""> <i class="fa fa-youtube"> </i> Youtube </a> </li>
+                    </ul>
+                  </div>
+                </aside>
+              </div>
+              <div class="col-lg-3 col-md-4  col-sm-12 col-12 dt-sc-one-fourth ">
+                <aside id="text-2" class="widget widget_text">
+                  <h3 class="widgettitle">About</h3>
+                    <div class="textwidget"><img title="DreamSpa" alt="DreamSpa" src="<?php echo get_template_directory_uri() .'/media/images/logo.png' ?>">
+                    <div class="dt-sc-hr-invisible-small"> </div>
+                    <p> Cannot foresee the pain and trouble that are bound to who fail in their duty through weakness of will, which is the same and pain.   </p>
+                  </div>
+                </aside>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="copyright">
+          <div class="container">
+            <div class="copyright-content">DreamSpa © 2016. Design by Design Themes</div>
+          </div>
+        </div>
+    </footer>
+  </div>
+ </div>
+ <style type="text/css">
+ 	.wpcp-carousel-section.nav-vertical-center {
+	  padding: 0px;
+	}
+	.slick-next {
+		margin-right: 10px;
+	}
+	.slick-prev{
+		margin-left: 10px;
+	}
+ </style>
+</div>
 </body>
 </html>
+<?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -15,45 +15,120 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="slider">
+	<?php echo do_shortcode('[sp_wpcarousel id="18"]'); ?>
+</div>
+<section id="primary" class="content-full-width">
+  <div class="dt-sc-hr-invisible "></div>
+  <div class="fullwidth-section">
+    <div class="container">
+      <div class="border-title aligncenter">
+        <h2>A Warm Welcome</h2>
+        <h6>We at Dream Spa provide various services to the nature of the clients. Wish how you would like to spend the</h6>
+        <h6>time here we can talk and come to a conclusion.</h6>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-lg-4 col-sm-12 col-12 dt-sc-one-third  space first">
+          <div class="dt-sc-ico-content type1 green">
+            <div class="icon">
+              <span> <img src="<?php echo get_template_directory_uri() .'/media/images/services-ico1.png' ?>" alt=""></span>
+            </div>
+            <h2>Ayurveda Spa</h2>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/services1.jpg' ?>" alt="" title="">
+            </div>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.</p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-sm-12 col-12  dt-sc-one-third  space ">
+          <div class="dt-sc-ico-content type1 chocolate">
+            <div class="icon">
+              <span> <img src="<?php echo get_template_directory_uri() .'/media/images/services-ico2.png' ?>" alt=""></span>
+            </div>
+            <h2>Luxury Spa</h2>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/services2.jpg' ?>" alt="" title="">
+            </div>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.</p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-sm-12 col-12  dt-sc-one-third  space ">
+          <div class="dt-sc-ico-content type1 blue">
+            <div class="icon">
+              <span> <img src="<?php echo get_template_directory_uri() .'/media/images/services-ico3.png' ?>" alt=""></span>
+            </div>
+            <h2>Massage Spa</h2>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/services3.jpg' ?>" alt="" title="">
+            </div>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.</p>
+          </div>
+        </div>
+      </div>
 
-		<?php
-		if ( have_posts() ) :
+    </div>
+  </div>
+  <div class="dt-sc-hr-invisible-medium"></div>
+  <div class="fullwidth-section skin-white">
+    <div class="container">
+      <div class="border-title aligncenter">
+        <h2>Popular Procedures</h2>
+        <h6>These procedures are the best and most loved by our customers.</h6>
+      </div>
+    <div class="row">
+      <div class="col-md-6 col-lg-4 col-sm-12 col-12 dt-sc-one-third  space first">
+        <div class="dt-sc-popular-procedures with-image">
+          <div class="dt-sc-popular-procedures-wrapper">
+            <div class="border"> </div>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/popular-procedures1.jpg' ?>" alt="Aromatheraphy">
+            </div>
+            <h2>Aromatheraphy</h2>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.</p>
+          </div>
+          <a href="#" class="dt-sc-button small">Read More</a>
+        </div>
+      </div>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+      <div class="col-md-6 col-lg-4 col-sm-12 col-12 dt-sc-one-third  space ">
+        <div class="dt-sc-popular-procedures with-image">
+          <div class="dt-sc-popular-procedures-wrapper">
+            <div class="border"> </div>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/popular-procedures2.jpg' ?>" alt="Skin Care">
+            </div>
+            <h2>Skin Care</h2>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.</p>
+          </div>
+          <a href="#" class="dt-sc-button small">Read More</a>
+        </div>
+      </div>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+      <div class="col-md-6 col-lg-4 col-sm-12 col-12 dt-sc-one-third  space ">
+        <div class="dt-sc-popular-procedures with-image">
+          <div class="dt-sc-popular-procedures-wrapper">
+            <div class="border"> </div>
+            <div class="image">
+              <img src="<?php echo get_template_directory_uri() .'/media/images/popular-procedures3.jpg' ?>" alt="Herbal Spa">
+            </div>
+            <h2>Herbal Spa</h2>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.</p>
+          </div>
+          <a href="#" class="dt-sc-button small">Read More</a>
+        </div>
+      </div>
+    </div>
+    <div class="dt-sc-hr-invisible-medium"></div>
+    <div class="border-title aligncenter">
+      <h2>Our Gallery</h2>
+      <h6>Take a look at our practice and process of defining spa!</h6>
+    </div>
+  </div>
+  <div class="slide-album">
+		 <?php echo do_shortcode( '[sp_wpcarousel id="22"]' ); ?>
+	</div>
+</div>
+</div>
+</section>
 <?php
-get_sidebar();
 get_footer();
